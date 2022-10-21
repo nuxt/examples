3a307a1e0e788a1dead50bef5c1477c3d4d7a94a
Sébastien Chopin <seb@nuxtlabs.com>
2022-10-21 10:51:03 +0200

    feat: add hello-world (#6)
    Co-authored-by: Daniel Roe <daniel@roe.dev>

---
+++ hello-world/app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const version = 2 + 1
+</script>
+
+<template>
+  <div class="hello">
+    Hello Nuxt {{ version }}!
+  </div>
+</template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
