af0e8b59b6e71123dc942ff639519ca6cccabd45
Sébastien Chopin <seb@nuxtlabs.com>
2023-02-06 19:39:06 +0100

    add(ui): TailwindCSS (#19)
    * add(ui): TailwindCSS
    
    * Update about.vue

---
+++ ui/tailwindcss/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="dark:bg-gray-800 dark:text-gray-200 min-h-screen">
+    <Head>
+      <Title>Nuxt with TailwindCSS</Title>
+    </Head>
+    <p class="p-4 pb-2">
+      <select
+        v-model="$colorMode.preference"
+        class="border w-24 h-8 dark:bg-gray-900 dark:text-white dark:border-gray-700"
+      >
+        <option value="system">System</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </p>
+    <NuxtPage class="mx-auto p-4" />
+  </div>
+</template>
---
+++ ui/tailwindcss/pages/about.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { theme } from '#tailwind-config'
+</script>
+
+<template>
+  <div class="p-4">
+    <h1 class="text-5xl">About page</h1>
+    <p class="my-4">
+      <NuxtLink to="/"><TButton>Home page</TButton></NuxtLink>
+    </p>
+    <h3 class="text-xl">Tailwind colors:</h3>
+    <pre class="bg-gray-200 dark:bg-gray-700 p-3 rounded">{{ JSON.stringify(theme.colors, null, 2) }}</pre>
+  </div>
+</template>
