abf84a9ea363ac6a32abcef543a9543a08b804d4
Sébastien Chopin <seb@nuxtjs.com>
2023-02-06 19:37:14 +0100

    add(ui): TailwindCSS

---
+++ ui/tailwindcss/nuxt.config.ts
@@ -0,0 +1,11 @@
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  colorMode: {
+    classSuffix: '',
+  },
+  // https://tailwindcss.nuxtjs.org
+  tailwindcss: {
+    // Expose tailwind config with #tailwind-config
+    exposeConfig: true
+  }
+})
---
+++ ui/tailwindcss/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div
+      class="max-w-sm rounded overflow-hidden shadow-lg mb-4 dark:bg-gray-900"
+    >
+      <img
+        class="w-full"
+        src="https://tailwindcss.com/img/card-top.jpg"
+        alt="Sunset in the mountains"
+      />
+      <div class="px-6 py-4 pb-2">
+        <div class="font-bold text-xl mb-2 dark:text-white">
+          The Coldest Sunset
+        </div>
+        <p class="text-gray-700 dark:text-gray-300 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
+      </div>
+      <div class="px-6 py-4">
+        <span class="badge mr-2">#photography</span>
+        <span class="badge mr-2">#travel</span>
+        <span class="badge">#winter</span>
+      </div>
+    </div>
+    <NuxtLink to="about">
+      <TButton>About</TButton>
+    </NuxtLink>
+  </div>
+</template>
