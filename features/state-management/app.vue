d5abc9c0a3b68824c0b022cd5ad9ba20ac2a10e4
Sébastien Chopin <seb@nuxt.com>
2023-06-19 13:45:33 +0200

    chore: move state management

---
+++ features/state-management/app.vue
@@ -0,0 +1,15 @@
+<script setup>
+const counter = useState('counter', () => Math.round(Math.random() * 1000))
+const sameCounter = useState('counter')
+</script>
+
+<template>
+  <div>
+    <p>Counter: {{ counter }}</p>
+    <p>
+      <button @click="counter--">-</button>
+      <button @click="counter++">+</button>
+    </p>
+    <p>Same Counter: {{ sameCounter }}</p>
+  </div>
+</template>
