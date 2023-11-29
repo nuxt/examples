fde6c6551cb6479c046c2d466020ac37c12a131d
Ali Afghah <ali.afghah@gmail.com>
2023-11-29 14:00:52 +0330

    reposition notifications

---
+++ auth/local/app.config.ts
@@ -0,0 +1,8 @@
+export default defineAppConfig({
+    ui: {
+        notifications: {
+            // Show toasts at the top right of the screen
+            position: 'top-0 bottom-auto'
+        }
+    }
+})
