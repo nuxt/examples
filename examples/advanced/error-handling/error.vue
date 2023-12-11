09df553da2a80aae69b3c8611f04a2a996893ce3
Arash <38922203+arashsheyda@users.noreply.github.com>
2023-12-11 11:54:44 -0700

    chore: remove n attribute

--- examples/advanced/error-handling/error.vue
+++ examples/advanced/error-handling/error.vue
@@ -10,7 +10,7 @@ function handleError() {
 
 <template>
   <NuxtExampleLayout>
-    <div class="relative font-sans" n="green6">
+    <div class="relative font-sans">
       <div class="container max-w-200 mx-auto py-10 px-4">
         <h1>{{ error?.message }}</h1>
         There was an error 😱
