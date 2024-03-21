d4d4f3a1b45ecbe836da7960d216e1f638e09e41
Daniel Roe <daniel@roe.dev>
2024-03-21 14:55:44 +0000

    chore: use `defineEventHandler`

--- examples/experimental/wasm/server/api/sum.ts
+++ examples/experimental/wasm/server/api/sum.ts
@@ -1,11 +1,11 @@
-import { defineLazyEventHandler } from 'h3'
+import { defineEventHandler, defineLazyEventHandler } from 'h3'
 
 export default defineLazyEventHandler(async () => {
     // @ts-expect-error TODO: https://github.com/nuxt/nuxt/issues/14131
   const { sum } = await import('~/server/wasm/sum.wasm')
 
-  return (event) => {
+  return defineEventHandler((event) => {
     const { a = 0, b = 0 } = getQuery(event)
     return { sum: sum(a, b) }
-  }
+  })
 })
