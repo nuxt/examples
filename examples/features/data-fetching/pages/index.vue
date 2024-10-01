2d1aa7a3b2f7a97d519ee68671a16d7add2dbcf8
Daniel Roe <daniel@roe.dev>
2024-10-01 08:50:41 +0100

    chore: use `value` prop to prevent newline

--- examples/features/data-fetching/pages/index.vue
+++ examples/features/data-fetching/pages/index.vue
@@ -5,9 +5,10 @@ const { data } = await useFetch('/api/hello')
 <template>
   <div>
     <p>
-      Result of <UKbd size="lg">
-        /api/hello
-      </UKbd>:
+      Result of <UKbd
+        size="lg"
+        value="/api/hello"
+      />:
     </p>
     <pre>{{ data }}</pre>
   </div>
