55704b2bbafe6d69d97ad5f6b7039e5dd039e2c4
Daniel Roe <daniel@roe.dev>
2025-04-03 12:16:43 +0100

    chore: update imports + add missing deps

--- examples/advanced/module-extend-pages/modules/pages/index.ts
+++ examples/advanced/module-extend-pages/modules/pages/index.ts
@@ -1,14 +1,14 @@
-import { defineNuxtModule, extendPages } from '@nuxt/kit'
-import { resolve } from 'pathe'
+import { createResolver, defineNuxtModule, extendPages } from 'nuxt/kit'
 
 export default defineNuxtModule({
   setup() {
+    const resolver = createResolver(import.meta.url)
     extendPages((pages) => {
       // Add /test page
       pages.push({
         name: 'Test',
         path: '/test',
-        file: resolve(__dirname, './pages/test.vue'),
+        file: resolver.resolve('./pages/test.vue'),
       })
     })
   },
