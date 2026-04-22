1d2412260decb7d7fad0c53e46125e22c82cd94f
Maxime Pauvert <maximepvrt@gmail.com>
2026-04-22 23:19:16 +0200

    udpate custom fetch

--- examples/advanced/use-custom-fetch-composable/composables/useCustomFetch.ts
+++ examples/advanced/use-custom-fetch-composable/composables/useCustomFetch.ts
@@ -1,11 +1,4 @@
-import type { UseFetchOptions } from 'nuxt/app'
-
-export function useCustomFetch<T>(
-  url: string | (() => string),
-  options: UseFetchOptions<T> = {},
-) {
-  return useFetch(url, {
-    ...options,
-    $fetch: useNuxtApp().$customFetch,
-  })
-}
+export const useCustomFetch = createUseFetch(callerOptions => ({
+  $fetch: useNuxtApp().$customFetch as typeof $fetch,
+  ...callerOptions,
+}))
